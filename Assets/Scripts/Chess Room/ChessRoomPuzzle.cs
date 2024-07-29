79614fe37071d0a1c8b5c1a2faf01e7329ae5566
rritsu <andrea_legaspi@dlsu.edu.ph>
2024-07-30 03:44:46 +0800

    piece prefabs and mats for room

--- Assets/Scripts/Chess Room/ChessRoomPuzzle.cs
+++ Assets/Scripts/Chess Room/ChessRoomPuzzle.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GreenRoomPuzzle : MonoBehaviour
+public class ChessRoomPuzzle : MonoBehaviour
 {
     [SerializeField]
     private GameObject Key;
